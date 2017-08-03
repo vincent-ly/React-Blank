94bf1286af19007818af26ba5dc515c75ef55870
vincent-ly <vincent_ly@outlook.com>
2017-08-02 22:23:41 -0700

    Update import path

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
-import Home from './components/home/home'
+import Home from 'components/home/home'
 
 const App = ({ children }) => (
 
